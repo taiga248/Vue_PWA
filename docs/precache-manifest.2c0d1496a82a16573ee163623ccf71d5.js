91c36be879ab0839f9192e6473155c7a22b97e96
Taiga <taiga248@gmail.com>
2020-06-02 02:15:33 +0900

    Add docs

---
+++ docs/precache-manifest.2c0d1496a82a16573ee163623ccf71d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddde18caf1a48a121582",
+    "url": "/Vue_PWA/css/app.d40fc157.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/Vue_PWA/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "994287fa526699f436f2071377a82770",
+    "url": "/Vue_PWA/index.html"
+  },
+  {
+    "revision": "ddde18caf1a48a121582",
+    "url": "/Vue_PWA/js/app.62a5d31e.js"
+  },
+  {
+    "revision": "c1845060b5ede70dfb86",
+    "url": "/Vue_PWA/js/chunk-vendors.21677903.js"
+  },
+  {
+    "revision": "da6ed7afc6e0c5b499ebca7f6a962ba0",
+    "url": "/Vue_PWA/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue_PWA/robots.txt"
+  }
+]);
