af284accc6a14e402030ad6bc8b0253c6793a6dc
Taiga <taiga248@gmail.com>
2020-06-02 02:16:20 +0900

    Add vue config

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,4 @@
 module.exports = {
-  // ...other vue-cli plugin options...
-  pwa: {
-    name: 'Vue PWA',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
-  }
+    publicPath: '/Vue_PWA',
+    outputDir: 'docs'
 }
